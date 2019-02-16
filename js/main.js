cee6cc0effa3e10bb36a1ed2451b8857f3265534
l <viroxic@gmail.com>
2019-02-16 16:16:08 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+(function() {
+    setTimeout(() => {
+        document.querySelector('main .loader').style.display = 'none';
+        document.querySelector('main .app').style.display = 'block';
+    }, 1000);
+ })();
